58ee1ace1c34c57a56d0b9dcd6027ed1b073ec38
p_startek <startekp98@gmail.com>
2025-11-16 14:08:32 +0100

    fix typo in dir path

--- 5_React_Framework_Architecture/2_hooks/app/(tabs)/index.tsx
+++ 5_React_Framework_Architecture/2_hooks/app/(tabs)/index.tsx
@@ -1,8 +1,8 @@
 import { StyleSheet } from 'react-native';
 import HooksExamples from '@/components/HooksExamples';
 import { View } from '@/components/Themed';
-import UserName from "@/Workspace/Zad4/UserName";
-import UserSurname from "@/Workspace/Zad4/UserSurname";
+import UserName from "@/Workspace/Zad5/UserName";
+import UserSurname from "@/Workspace/Zad5/UserSurname";
 
 export default function TabOneScreen() {
   return (
